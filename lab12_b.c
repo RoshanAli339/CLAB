be0ff9d3e31025af1109a28368ad8e899cdd4d55
RoshanAli339 <roshanali339lps@gmail.com>
2022-04-01 22:03:04 +0530

    Friday 01 April 2022 10:03:04 PM IST

---
+++ lab12_b.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Required number of arguments are not given!\n");
+        return 0;
+    }
+
+    FILE *f1 = fopen(argv[1], "r");
+    FILE *f2 = fopen(argv[2], "w");
+
+    char ch;
+    while ((ch = fgetc(f1))!=EOF)
+    {
+        fputc(ch, f2);
+    }
+
+    fclose(f1);
+    fclose(f2);
+}
