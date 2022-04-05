77500815679a069b8b6acc96730c91f1ac41faf2
RoshanAli339 <roshanali339lps@gmail.com>
2022-04-05 16:47:16 +0530

    Tuesday 05 April 2022 04:47:16 PM IST

--- lab12_b.c
+++ lab12_b.c
@@ -9,6 +9,12 @@ int main(int argc, char *argv[])
     }
 
     FILE *f1 = fopen(argv[1], "r");
+    if (f1 == NULL)
+    {
+        printf("Unabel to open file!\n");
+        return 1;
+    }
+
     FILE *f2 = fopen(argv[2], "w");
 
     char ch;
